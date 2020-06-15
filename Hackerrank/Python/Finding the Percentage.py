9591a95b1964cad47914cac2252713cf98f8ff18
Nav-31 <60818428+Nav-31@users.noreply.github.com>
2020-06-15 13:07:49 +0530

    %
    Input as 2d array n den store d result in dictionary data type.
    set precision to 2

---
+++ Hackerrank/Python/Finding the Percentage.py
@@ -0,0 +1,29 @@
+try:
+    n = int(input("Enter the no. of students:"))
+    
+except:
+    print("ERROR! Enter numeric input only")
+    quit()
+
+record=[]
+for i in range(n):
+    r=list(input().split())         #Enter the name and 3 scores separated by spaces
+    record.append(r)                #eg: Navya 25 24 21
+
+p=[]
+for i in range(n):
+    avg=0
+    for j in range(1,4):
+        avg=avg+int(record[i][j])
+    avg= (avg/3)
+    p.append(avg)
+
+
+database={}
+for i in range(n):
+    database[record[i][0]]=p[i]
+
+print(database)
+i=input("Display d avg of:")
+x=float(database[i])
+print('%.2f'%x)                     #setting precision to 2 decimal places
